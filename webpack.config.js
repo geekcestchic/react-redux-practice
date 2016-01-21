1d1c4782999c70d45ff9572c55eff9cc1a77f0f1
Alex Howard <alexkarimhoward@gmail.com>
2016-01-21 16:34:14 +0000

    created filtered list with react and redux

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path')
+
+module.exports = {
+    entry: "./lib/web/main.js",
+    output: {
+        path: __dirname,
+        filename: "./lib/web/bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+        ]
+    }
+};
